51104a70c8daee2e18b94072356a7cd0ff4052b8
LaurentMaxhuni <152623037+LaurentMaxhuni@users.noreply.github.com>
2025-11-13 00:24:14 +0100

    Added some new skills. (ofc)

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import SkillsCard from "./SkillsCard";
 import html from "../assets/icons/html.svg";
 import css from "../assets/icons/css.svg";
@@ -12,6 +12,10 @@ import tailwind from "../assets/icons/tailwind-css.svg";
 import bootstrap from "../assets/icons/bootstrap.svg";
 import jquery from "../assets/icons/jquery.svg";
 import node from "../assets/icons/node.svg";
+import vue from "../assets/icons/vue.svg";
+import angular from "../assets/icons/angular.svg";
+import express from "../assets/icons/express.svg";
+import mysql from "../assets/icons/mysql.svg";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -60,59 +64,87 @@ function Skills() {
             delay="400"
           />
           <SkillsCard
+            image={vue}
+            title="Vue JS"
+            description="I have experience with Vue.js, allowing me to build smooth, reactive user interfaces with a simple and flexible workflow."
+            link="https://vuejs.org/"
+            delay="100"
+          />
+          <SkillsCard
+            image={angular}
+            title="Angular JS"
+            description="I have experience with Angular, enabling me to create scalable front-end applications using components and TypeScript."
+            link="https://angular.dev/"
+            delay="200"
+          />
+          <SkillsCard
             image={python}
             title="Python"
             description="I have a good understanding of Python and its syntax, enabling me to write clean and readable code."
             link="https://www.python.org/"
-            delay="100"
+            delay="300"
           />
           <SkillsCard
             image={git}
             title="Git"
             description="I have experience with Git and version control, allowing me to collaborate with other developers and manage code effectively."
             link="https://git-scm.com/"
-            delay="200"
+            delay="400"
           />
           <SkillsCard
             image={github}
             title="GitHub"
             description="I have experience with GitHub and its features, enabling me to host projects and collaborate with other developers."
             link="https://github.com/"
-            delay="300"
+            delay="100"
           />
           <SkillsCard
             image={vscode}
             title="VSCode"
             description="I have experience with Visual Studio Code, enabling me to write code efficiently and use various extensions for development."
             link="https://code.visualstudio.com/"
-            delay="400"
+            delay="200"
           />
           <SkillsCard
             image={tailwind}
             title="Tailwind CSS"
             description="I have experience with Tailwind CSS, enabling me to create responsive and visually appealing user interfaces."
             link="https://tailwindcss.com/"
-            delay="100"
+            delay="300"
           />
           <SkillsCard
             image={bootstrap}
             title="Bootstrap 5"
             description="I have experience with Bootstrap 5, enabling me to create responsive and mobile-first designs quickly."
             link="https://getbootstrap.com/"
-            delay="200"
+            delay="400"
           />
           <SkillsCard
             image={jquery}
             title="jQuery"
             description="I have experience with jQuery, enabling me to add interactivity and functionality to web pages."
             link="https://jquery.com/"
-            delay="300"
+            delay="100"
           />
           <SkillsCard
             image={node}
             title="Node.js"
-            description="I have experience with Node.js, enabling me to build server-side applications with JavaScript."  
+            description="I have experience with Node.js, enabling me to build server-side applications with JavaScript."
             link="https://nodejs.org/en/"
+            delay="200"
+          />
+          <SkillsCard
+            image={express}
+            title="Express.js"
+            description="I have experience with Express.js, allowing me to build backend APIs, manage routes, and handle server logic efficiently."
+            link="https://expressjs.com/"
+            delay="300"
+          />
+          <SkillsCard
+            image={mysql}
+            title="MySQL"
+            description="I have experience with MySQL, enabling me to design and manage relational databases for web applications."
+            link="https://www.mysql.com/"
             delay="400"
           />
         </div>
--- src/components/SkillsLevel.jsx
+++ src/components/SkillsLevel.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import html from "../assets/icons/html.svg";
 import css from "../assets/icons/css.svg";
 import js from "../assets/icons/javascript.svg";
@@ -11,6 +10,10 @@ import tailwind from "../assets/icons/tailwind-css.svg";
 import bootstrap from "../assets/icons/bootstrap.svg";
 import jquery from "../assets/icons/jquery.svg";
 import node from "../assets/icons/node.svg";
+import vue from "../assets/icons/vue.svg";
+import angular from "../assets/icons/angular.svg";
+import express from "../assets/icons/express.svg";
+import mysql from "../assets/icons/mysql.svg";
 import SkillLevelBar from "./SkillLevelBar";
 
 function SkillsLevel() {
@@ -26,9 +29,11 @@ function SkillsLevel() {
         </h1>
         <div className="row my-5">
           <SkillLevelBar title="HTML" image={html} percentage={100} />
-          <SkillLevelBar title="CSS" image={css} percentage={90} />
+          <SkillLevelBar title="CSS" image={css} percentage={95} />
           <SkillLevelBar title="JavaScript" image={js} percentage={85} />
           <SkillLevelBar title="React JS" image={react} percentage={75} />
+          <SkillLevelBar title="Vue JS" image={vue} percentage={65} />
+          <SkillLevelBar title="Angular JS" image={angular} percentage={55} />
           <SkillLevelBar title="Python" image={python} percentage={50} />
           <SkillLevelBar title="Git" image={git} percentage={60} />
           <SkillLevelBar title="GitHub" image={github} percentage={60} />
@@ -40,7 +45,9 @@ function SkillsLevel() {
           />
           <SkillLevelBar title="Bootstrap" image={bootstrap} percentage={95} />
           <SkillLevelBar title="jQuery" image={jquery} percentage={65} />
-          <SkillLevelBar title="Node JS" image={node} percentage={20} />
+          <SkillLevelBar title="Node JS" image={node} percentage={70} />
+          <SkillLevelBar title="Express JS" image={express} percentage={70} />
+          <SkillLevelBar title="MySQL" image={mysql} percentage={65} />
         </div>
       </div>
     </section>
